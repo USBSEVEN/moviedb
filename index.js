1f7664cefb045eed20e71eb8b282b23149c4a2e9
USBSEVEN <98146649+USBSEVEN@users.noreply.github.com>
2023-05-12 20:18:19 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/movie', (req, res, next) =>{
 app.get('/movie/:number', function (req, res, next) {
   const id = req.params.id;
   connection.query(
-    'SELECT * FROM `movie` WHERE `number` = ?',
+    'SELECT * FROM movie WHERE number = ?',
     [number],
     function(err, results) {
       res.json(results);
